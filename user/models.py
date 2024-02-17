3e5c5c2443d9d668343c60f1e7bf164658d5530d
Dhara Patel <pateldharapravin@gmail.com>
2024-02-17 16:42:18 -0500

    modified user models

--- user/models.py
+++ user/models.py
@@ -1,4 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+
+def avatar_path(instance, filename):
+    return 'avatar/{}/{}'.format(
+        instance.id,
+        filename
+    )
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not username:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            username=self.normalize_email(username),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            username=username,
+            password=password,
+        )
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class UserType(models.Model):
@@ -6,35 +47,23 @@ class UserType(models.Model):
         ('user', 'User'),
         ('service_provider', 'Service Provider')
     )
-    user_type = models.CharField(max_length=100, choices=USER_TYPE_CHOICES, default='user', null=False)
+    user_type = models.CharField(max_length=100, choices=USER_TYPE_CHOICES, default='user', null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.user_type
 
-    def __repr__(self):
-        return f'<UserType: {self.user_type}>'
-
-
-class User(models.Model):
-    user_type = models.ForeignKey(UserType, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    user_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=30)
-    phone_number = models.CharField(max_length=20)
+class User(AbstractBaseUser):
+    user_type = models.ForeignKey(UserType, on_delete=models.CASCADE, null=False, blank=False)
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    username = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=30, null=False, blank=False, unique=True)
+    phone_number = models.CharField(max_length=20, null=False, blank=False, unique=True)
     password = models.CharField(max_length=128)
-    profile_picture = models.ImageField(upload_to='media/profile_pictures/', null=True, blank=True)
+    avatar = models.ImageField(upload_to=avatar_path, blank=True, null=True)
     availability = models.FloatField(default=0.0)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     rating = models.FloatField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-    def __repr__(self):
-        return f'<User: {self.user_name}>'
