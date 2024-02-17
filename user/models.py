3f9e05ae4b4477706bd5b277d7e15e8b571b1d10
Rana Shivani <51319420+ShivaniRana0603@users.noreply.github.com>
2024-02-17 16:45:11 -0400

    Update models.py
    models created for feedback and user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from datetime import datetime
+
+
+class UserAddress:
+    def __init__(self, add1: str, add2: str, user: str, city: str, provision: str, country: str,
+                 postal_code: str, created_at: datetime, updated_at: datetime):
+        self.add1 = add1
+        self.add2 = add2
+        self.user = user
+        self.city = city
+        self.provision = provision
+        self.country = country
+        self.postal_code = postal_code
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+
+class Feedback:
+    def __init__(self, user: str, feedback: str, created_at: datetime, updated_at: datetime):
+        self.user = user
+        self.feedback = feedback
+        self.created_at = created_at
+        self.updated_at = updated_at
