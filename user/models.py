6b1b0672a270b5ea9f2a21dc79eaccf199a3fd0d
Dhara Patel <pateldharapravin@gmail.com>
2024-02-17 16:13:34 -0500

    Added field profile_pictures and setup path for media

--- user/models.py
+++ user/models.py
@@ -20,6 +20,7 @@ class User(models.Model):
     email = models.EmailField(max_length=30)
     phone_number = models.CharField(max_length=20)
     password = models.CharField(max_length=128)
+    profile_picture = models.ImageField(upload_to='media/profile_pictures/', null=True, blank=True)
     availability = models.FloatField(default=0.0)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
