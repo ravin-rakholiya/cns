c6feb092e10904247d251f2cf9a1f69cec73ea17
darsh <darshwork008@gmail.com>
2024-03-10 18:02:57 -0400

    admin-update

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from service.models import *
 # Register your models here.
+admin.site.register(UserServiceRating)
+admin.site.register(UserService)
+admin.site.register(ServicePost)
+admin.site.register(ServiceCategory)
