c13934e9bd22555c46b50bcc372ffd683c79ccb8
Pratima <pratimabhatia.pb@gmail.com>
2024-03-15 19:18:10 -0400

    path fixed for redirection

--- user/urls.py
+++ user/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('choose_register', choose_register, name='choose_register'),  # Add this line
     path('provide_signup', provide_signup, name='provide_signup'),
     path('user_signup', user_signup, name='user_signup'),
-    path('forgot_password', ForgotPasswordPage.as_view(), name='forgot_password'),
-    path('password_reset', PasswordResetView.as_view(), name='password_reset'),
+    path('forgot_password', forgot_password, name='forgot_password'),
+    path('reset_password', reset_password, name='reset_password'),
+
 ]
     # path('generate_otp', views.GenerateOTP.as_view(), name='generate-otp'),
