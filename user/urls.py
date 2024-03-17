3557ecf89830e8e8e716380ca4507d0ad4b59a17
ravin-rakholiya <ravinkumarrakh@gmail.com>
2024-03-17 18:06:44 -0400

    created saperated folders for customer and provider

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,8 @@ from user.views import *
 app_name = 'user'
 
 urlpatterns = [
+
+    # Auth Routs
     path('', index, name='index'),
     path('index', index, name='index_html'),  # Add this line
     path('choose_register', choose_register, name='choose_register'),  # Add this line
@@ -12,5 +14,8 @@ urlpatterns = [
     path('forgot_password', forgot_password, name='forgot_password'),
     path('reset_password', reset_password, name='reset_password'),
     path('user_signin', user_signin, name='user_signin'),
-   
+
+
+    # Provider Routs
+    path('provider_dashboard', provider_dashboard, name='provider_dashboard'),
 ]
--- user/views.py
+++ user/views.py
@@ -31,90 +31,6 @@ def user_signup(request):
 def user_signin(request):
     return render(request, 'login/login.html')
 
-# Create your views here.
-# def signup(request):
-#     if request.method == 'POST':
-#         # Process the signup form data here
-#         return HttpResponse('Signup successful!')
-#     else:
-#         return render(request, 'user/signup.html')
-
-
-# def login(request):
-#     if request.method == 'POST':
-#         email = request.POST.get('email')
-#         password = request.POST.get('password')
-#         remember_me = request.POST.get('remember_me', False)
-
-#         user = Login_main(
-#             email=email,
-#             password=password,
-#             remember_me=remember_me,
-#         )
-#         user.save()
-
-#         return render(request, 'user/index.html')
-#     return render(request, 'user/index.html')
-
-
-# def choose_signup(request):
-#     return render(request, 'choose_signup.html')
-
-
-# def user_signup(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         email = request.POST.get('email')
-#         phone = request.POST.get('phone')
-#         password = request.POST.get('password')
-
-#         # Create a new User_s object and save it to the database
-
-#         user = UserSignup(name=name, email=email, phone=phone, password=password)
-#         user.save()
-
-#         # Redirect the user to a different page after signup
-#         return redirect('user:index')
-#     else:
-#         return render(request, 'user/signup.html')
-
-
-# def Login_main(request):
-#     if request.method == 'POST':
-#         email = request.POST.get('email')
-#         password = request.POST.get('password')
-
-#         # Check if the email and password match any user in UserSignup table
-#         try:
-#             user = UserSignup.objects.get(email=email)
-#         except UserSignup.DoesNotExist:
-#             user = None
-
-#         if user is not None and check_password(password, user.password):
-#             # User is found in UserSignup table, log them in
-#             # Implement your login logic here
-#             return redirect('user:index')
-
-#         # If user is not found in UserSignup table, check ProviderSignup table
-#         try:
-#             provider = Provider.objects.get(email=email)
-#         except Provider.DoesNotExist:
-#             provider = None
-
-#         if provider is not None and check_password(password, provider.password):
-#             # Provider is found in ProviderSignup table, log them in
-#             # Implement your login logic here
-#             return redirect('provider:index')
-
-#         # If neither user nor provider is found, show login error
-#         return render(request, 'user/Login_main.html', {'error': 'Invalid email or password'})
-
-#     return render(request, 'user/Login_main.html')
-
-
-# def servicelist(request):
-#     return render(request, 'service/service_listing.html')
-
 def forgot_password(request):
     if request.method == 'POST':
         form = ForgotPasswordForm(request.POST)
@@ -129,3 +45,6 @@ def forgot_password(request):
 
 def reset_password(request):
     return render(request, 'login/reset_password.html')
+
+def provider_dashboard(request):
+    return render(request, 'provider/provider-dashboard.html')
