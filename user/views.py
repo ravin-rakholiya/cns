5202e414e1b06605889910905cb806423089742c
Pratima <pratimabhatia.pb@gmail.com>
2024-03-17 22:51:57 -0400

    Created view for creating a new customers profile

--- user/views.py
+++ user/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy, reverse
 
 from cns import settings
 from service.models import Provider
-from user.forms import ForgotPasswordForm
+from user.forms import ForgotPasswordForm, UserProfileForm, AddressForm
 from user.models import User, UserSignup, Login_main
 
 from django.http import HttpResponseRedirect
@@ -28,8 +28,10 @@ def provide_signup(request):
 def user_signup(request):
     return render(request, 'register/user-signup.html')
 
+
 def user_signin(request):
     return render(request, 'login/login.html')
+
 
 def forgot_password(request):
     if request.method == 'POST':
@@ -46,5 +48,26 @@ def forgot_password(request):
 def reset_password(request):
     return render(request, 'login/reset_password.html')
 
+
 def provider_dashboard(request):
     return render(request, 'provider/provider-dashboard.html')
+
+
+def customer_profile_creation(request):
+    if request.method == 'POST':
+        user_form = UserProfileForm(request.POST)
+        address_form = AddressForm(request.POST)
+        if user_form.is_valid() and address_form.is_valid():
+            user = user_form.save(commit=False)
+            address = address_form.save(commit=False)
+            user.save()
+            address.user = user
+            address.save()
+            return redirect('success_page')
+    else:
+        user_form = UserProfileForm()
+        address_form = AddressForm()
+    return render(request, 'customer/customer_profile_creation.html', {'user_form': user_form, 'address_form': address_form})
+
+
+
