eb8115831246d060fb3a39c8b2a6fa7b81acdd55
Pratima <pratimabhatia.pb@gmail.com>
2024-03-17 22:57:46 -0400

    Created form for fetching user details and address details and also added required widgets to it

--- user/forms.py
+++ user/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import PasswordResetForm
 
-from user.models import User
+from user.models import User, UserAddress
 
 
 class UserSignupForm(forms.ModelForm):
@@ -13,4 +13,40 @@ class UserSignupForm(forms.ModelForm):
 
 
 class ForgotPasswordForm(forms.Form):
-    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'johndoe@example.com'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': 'johndoe@example.com'}))
+
+
+class UserProfileForm(forms.ModelForm):
+    GENDER_CHOICES = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+    gender = forms.ChoiceField(choices=GENDER_CHOICES)
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    currency_code = forms.ChoiceField(choices=[('USD', 'USD'), ('EUR', 'EUR'), ('GBP', 'GBP')])  # Add currency dropdown
+    language = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Enter Language...'}))  # Use TextInput for language input
+    profile_picture = forms.ImageField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'username', 'email', 'gender', 'phone_number', 'date_of_birth', 'currency_code',
+                  'language']
+        widgets = {
+            field_name: forms.TextInput(
+                attrs={'class': 'form-control form-group col-md-6 col-form-label"',
+                       'placeholder': f'Enter {field_name.capitalize()}'})
+            for field_name in fields
+        }
+
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = UserAddress
+        fields = ['add1', 'add2', 'city', 'provision', 'country', 'postal_code']
+        widgets = {
+            field_name: forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': f'Enter {field_name.capitalize()}'})
+            for field_name in fields
+        }
