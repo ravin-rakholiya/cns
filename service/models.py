3ca7a8dcb4f363661c663f1a27ddfd5409bc3635
Dhara Patel <pateldharapravin@gmail.com>
2024-03-17 20:41:02 -0400

    updated service post model

--- service/models.py
+++ service/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-
 # Create your models here.
 class ServiceCategory(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
@@ -13,17 +12,48 @@ class ServiceCategory(models.Model):
         return self.id
 
 
+class Country(models.Model):
+    country = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.country
+
+
+class State(models.Model):
+    state = models.CharField(max_length=20)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.state
+
+
+class City(models.Model):
+    city = models.CharField(max_length=20)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.city
+
+
 class ServicePost(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
     category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE, null=False, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     desc = models.TextField(null=True, blank=True)
-    per_hrs_rate = models.FloatField(default=0)
-    hrs_of_work = models.FloatField(default=0)
+    price = models.FloatField(default=0)
+    duration = models.FloatField(default=0)
+    address = models.CharField(max_length=100, null=False, blank=False, default='')  # Set default value here
+    country = models.CharField(max_length=100, null=False, blank=False)
+    city = models.CharField(max_length=50, null=False, blank=False)
+    state = models.CharField(max_length=100, null=False, blank=False)
+    pincode = models.CharField(max_length=100, null=False, blank=False)
+    picture = models.ImageField(upload_to='services', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
+
 
 
 class UserService(models.Model):
@@ -58,6 +88,7 @@ class Provider(models.Model):
 
     def __str__(self):
         return self.id
+
 
 class ServicePostComment(models.Model):
     comment = models.TextField(null=False, blank=False)
